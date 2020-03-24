0469b603ef5946a79a315150393df2a40ecbaa97
rahimeoksuzz <61289562+rahimeoksuzz@users.noreply.github.com>
2020-03-24 22:22:12 +0300

    Update hw02.h

--- hw02.h
+++ hw02.h
@@ -25,15 +25,15 @@ public:
 
 	complex(double a, double b) ; //İki parametre alan constructor
 	
-	double getReal()const;   //reel değeri tutan özel veri üyelerine erişen fonksiyon 
+	double getReal()const;   //gerçel değeri tutan özel veri üyelerine erişen fonksiyon 
 	double getImag()const;   //sanal değeri tutan özel veri üyelerine erişen fonksiyon 
 	
-	void setReal(double val);   //reel değeri düzenlemek için fonksiyon  
+	void setReal(double val);   //gerçel değeri düzenlemek için fonksiyon  
 	void setImag(double val);   //sanal değeri düzenlemek için fonksiyon  
 	
 	void add(complex &x); //toplama fonksiyonu
 	void subtract(complex &x); // çıkarma fonksiyonu
-    void divide(complex &x); // çarpma fonksiyonu
+        void divide(complex &x); // bölme fonksiyonu
 
 	void print()const; // print fonksiyonu
 
